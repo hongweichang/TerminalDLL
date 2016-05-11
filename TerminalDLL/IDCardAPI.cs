3acb8056d230de4544dbd3c0c5712d67a923bc98
bigface <812166094@qq.com>
2016-05-11 17:20:20 +0800

    First commit,Add IdCardAPI.

---
+++ TerminalDLL/IDCardAPI.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Text;
+
+
+namespace TerminalDLL
+{
+    public class IDCardAPI
+    {
+        [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Ansi, Pack = 1)]
+        public struct IDCardData
+        {
+            [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 32)]
+            public string Name; //姓名   
+            [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 6)]
+            public string Sex;   //性别
+            [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 20)]
+            public string Nation; //名族
+            [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 18)]
+            public string Born; //出生日期
+            [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 72)]
+            public string Address; //住址
+            [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 38)]
+            public string IDCardNo; //身份证号
+            [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 32)]
+            public string GrantDept; //发证机关
+            [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 18)]
+            public string UserLifeBegin; // 有效开始日期
+            [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 18)]
+            public string UserLifeEnd;  // 有效截止日期
+            [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 38)]
+            public string reserved; // 保留
+            [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 255)]
+            public string PhotoFileName; // 照片路径
+        }
+        /************************端口类API *************************/
+        [DllImport("SynIDCardAPI.dll", EntryPoint = "Syn_GetCOMBaud", CharSet = CharSet.Ansi)]
+        public static extern int Syn_GetCOMBaud(int iPort, ref uint puiBaudRate);
+        [DllImport("SynIDCardAPI.dll", EntryPoint = "Syn_SetCOMBaud", CharSet = CharSet.Ansi)]
+        public static extern int Syn_SetCOMBaud(int iPort, uint uiCurrBaud, uint uiSetBaud);
+        [DllImport("SynIDCardAPI.dll", EntryPoint = "Syn_OpenPort", CharSet = CharSet.Ansi)]
+        public static extern int Syn_OpenPort(int iPort);
+        [DllImport("SynIDCardAPI.dll", EntryPoint = "Syn_ClosePort", CharSet = CharSet.Ansi)]
+        public static extern int Syn_ClosePort(int iPort);
+        /**************************SAM类函数 **************************/
+        [DllImport("SynIDCardAPI.dll", EntryPoint = "Syn_SetMaxRFByte", CharSet = CharSet.Ansi)]
+        public static extern int Syn_SetMaxRFByte(int iPort, byte ucByte, int iIfOpen);
+        [DllImport("SynIDCardAPI.dll", EntryPoint = "Syn_ResetSAM", CharSet = CharSet.Ansi)]
+        public static extern int Syn_ResetSAM(int iPort, int iIfOpen);
+        [DllImport("SynIDCardAPI.dll", EntryPoint = "Syn_GetSAMStatus", CharSet = CharSet.Ansi)]
+        public static extern int Syn_GetSAMStatus(int iPort, int iIfOpen);
+        [DllImport("SynIDCardAPI.dll", EntryPoint = "Syn_GetSAMID", CharSet = CharSet.Ansi)]
+        public static extern int Syn_GetSAMID(int iPort, ref byte pucSAMID, int iIfOpen);
+        [DllImport("SynIDCardAPI.dll", EntryPoint = "Syn_GetSAMIDToStr", CharSet = CharSet.Ansi)]
+        public static extern int Syn_GetSAMIDToStr(int iPort, ref byte pcSAMID, int iIfOpen);
+        /*************************身份证卡类函数 ***************************/
+        [DllImport("SynIDCardAPI.dll", EntryPoint = "Syn_StartFindIDCard", CharSet = CharSet.Ansi)]
+        public static extern int Syn_StartFindIDCard(int iPort, ref byte pucIIN, int iIfOpen);
+        [DllImport("SynIDCardAPI.dll", EntryPoint = "Syn_SelectIDCard", CharSet = CharSet.Ansi)]
+        public static extern int Syn_SelectIDCard(int iPort, ref byte pucSN, int iIfOpen);
+        [DllImport("SynIDCardAPI.dll", EntryPoint = "Syn_ReadBaseMsg", CharSet = CharSet.Ansi)]
+        public static extern int Syn_ReadBaseMsg(int iPort, ref byte pucCHMsg, ref uint puiCHMsgLen, ref byte pucPHMsg, ref uint puiPHMsgLen, int iIfOpen);
+        [DllImport("SynIDCardAPI.dll", EntryPoint = "Syn_ReadBaseMsgToFile", CharSet = CharSet.Ansi)]
+        public static extern int Syn_ReadBaseMsgToFile(int iPort, ref byte pcCHMsgFileName, ref uint puiCHMsgFileLen, ref byte pcPHMsgFileName, ref uint puiPHMsgFileLen, int iIfOpen);
+        [DllImport("SynIDCardAPI.dll", EntryPoint = "Syn_ReadBaseFPMsg", CharSet = CharSet.Ansi)]
+        public static extern int Syn_ReadBaseFPMsg(int iPort, ref byte pucCHMsg, ref uint puiCHMsgLen, ref byte pucPHMsg, ref uint puiPHMsgLen, ref byte pucFPMsg, ref uint puiFPMsgLen, int iIfOpen);
+        [DllImport("SynIDCardAPI.dll", EntryPoint = "Syn_ReadBaseFPMsgToFile", CharSet = CharSet.Ansi)]
+        public static extern int Syn_ReadBaseFPMsgToFile(int iPort, ref byte pcCHMsgFileName, ref uint puiCHMsgFileLen, ref byte pcPHMsgFileName, ref uint puiPHMsgFileLen, ref byte pcFPMsgFileName, ref uint puiFPMsgFileLen, int iIfOpen);
+        [DllImport("SynIDCardAPI.dll", EntryPoint = "Syn_ReadNewAppMsg", CharSet = CharSet.Ansi)]
+        public static extern int Syn_ReadNewAppMsg(int iPort, ref byte pucAppMsg, ref uint puiAppMsgLen, int iIfOpen);
+        [DllImport("SynIDCardAPI.dll", EntryPoint = "Syn_GetBmp", CharSet = CharSet.Ansi)]
+        public static extern int Syn_GetBmp(int iPort, ref byte Wlt_File);
+        [DllImport("SynIDCardAPI.dll", EntryPoint = "Syn_ReadMsg", CharSet = CharSet.Ansi)]
+        public static extern int Syn_ReadMsg(int iPortID, int iIfOpen, ref IDCardData pIDCardData);
+        [DllImport("SynIDCardAPI.dll", EntryPoint = "Syn_ReadFPMsg", CharSet = CharSet.Ansi)]
+        public static extern int Syn_ReadFPMsg(int iPortID, int iIfOpen, ref IDCardData pIDCardData, ref byte cFPhotoname);
+
+        /// <summary>
+        /// 寻找读卡器
+        /// </summary>
+        /// <returns></returns>
+        [DllImport("SynIDCardAPI.dll", EntryPoint = "Syn_FindReader", CharSet = CharSet.Ansi)]
+        public static extern int Syn_FindReader();
+        [DllImport("SynIDCardAPI.dll", EntryPoint = "Syn_FindUSBReader", CharSet = CharSet.Ansi)]
+        public static extern int Syn_FindUSBReader();
+        /***********************设置附加功能函数 ************************/
+        [DllImport("SynIDCardAPI.dll", EntryPoint = "Syn_SetPhotoPath", CharSet = CharSet.Ansi)]
+        public static extern int Syn_SetPhotoPath(int iOption, ref byte cPhotoPath);
+        [DllImport("SynIDCardAPI.dll", EntryPoint = "Syn_SetPhotoType", CharSet = CharSet.Ansi)]
+        public static extern int Syn_SetPhotoType(int iType);
+        [DllImport("SynIDCardAPI.dll", EntryPoint = "Syn_SetPhotoName", CharSet = CharSet.Ansi)]
+        public static extern int Syn_SetPhotoName(int iType);
+        [DllImport("SynIDCardAPI.dll", EntryPoint = "Syn_SetSexType", CharSet = CharSet.Ansi)]
+        public static extern int Syn_SetSexType(int iType);
+        [DllImport("SynIDCardAPI.dll", EntryPoint = "Syn_SetNationType", CharSet = CharSet.Ansi)]
+        public static extern int Syn_SetNationType(int iType);
+        [DllImport("SynIDCardAPI.dll", EntryPoint = "Syn_SetBornType", CharSet = CharSet.Ansi)]
+        public static extern int Syn_SetBornType(int iType);
+        [DllImport("SynIDCardAPI.dll", EntryPoint = "Syn_SetUserLifeBType", CharSet = CharSet.Ansi)]
+        public static extern int Syn_SetUserLifeBType(int iType);
+        [DllImport("SynIDCardAPI.dll", EntryPoint = "Syn_SetUserLifeEType", CharSet = CharSet.Ansi)]
+        public static extern int Syn_SetUserLifeEType(int iType, int iOption);
+    }
+}
